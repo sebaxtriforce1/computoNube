54baa8da6bff7c67f14da7c3a50ce8da3f5d8472
sebaxtriforce1 <LC18280838@toluca.tecnm.mx>
2024-06-18 11:17:39 -0600

    Update admin.py

--- app_chat/admin.py
+++ app_chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Carreras,Persona,Mensajes
 # Register your models here.
+admin.site.register(Carreras)
+admin.site.register(Persona)
+admin.site.register(Mensajes)
