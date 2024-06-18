145f3c75d7c89c46dc0b90804802a33ee94a384c
sebaxtriforce1 <LC18280838@toluca.tecnm.mx>
2024-06-18 12:06:16 -0600

    Update urls.py

--- app_p/urls.py
+++ app_p/urls.py
@@ -20,7 +20,7 @@ from app_bienvenida.views import bienvenida
 from app_chat import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('app-jango.azurewebsites.net')),
     path('',bienvenida),
     path('mensajes/', views.consultar, name='mensajes'),
 ]
